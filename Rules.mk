7db2874373eaef01f9323b37a25d61d072d07c33
Shiqing <shiqing-thu18@yandex.com>
2020-01-02 22:48:45 +0800

    Fix 'selected processor lacks an FPU' for Pi 0/1

--- Rules.mk
+++ Rules.mk
@@ -41,7 +41,7 @@ AR	= $(PREFIX)ar
 
 ifeq ($(strip $(AARCH64)),0)
 ifeq ($(strip $(RASPPI)),1)
-ARCH	?= -march=armv6j -mtune=arm1176jzf-s -mfloat-abi=hard 
+ARCH	?= -march=armv6j -mtune=arm1176jzf-s -mfloat-abi=hard -mfpu=vfp
 TARGET	?= kernel
 else ifeq ($(strip $(RASPPI)),2)
 ARCH	?= -march=armv7-a -mtune=cortex-a7 -mfloat-abi=hard
